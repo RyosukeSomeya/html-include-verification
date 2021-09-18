86aaf23558e739fe967ca4cc713448d0d5f9c81a
RyosukeSomeya <rx.78.0618@gmail.com>
2021-09-18 16:50:31 +0900

    httpリクエスト部分を修正

--- js/include.js
+++ js/include.js
@@ -3,7 +3,13 @@ function include(component){
     const target = document.getElementById(idName);
 
     const xml = new XMLHttpRequest();
-    xml.onreadystatechange = () => {
+    // リクエスト設定
+    xml.open("GET", `../include/${component}.html`, true);
+    // リクエスト送信
+    xml.send(null);
+
+    // 通信完了時に呼び出し
+    xml.onload = () => {
         if (xml.readyState === 4) {
             if (xml.status == 200) {
                 target.innerHTML = xml.responseText;
@@ -12,7 +18,4 @@ function include(component){
             }
         }
     }
-    xml.open("GET", `../include/${component}.html`, true);
-
-    xml.send(null);
 }
